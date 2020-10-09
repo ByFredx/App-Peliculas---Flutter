ca2e18b3eb8293f2720488513416681b75681ad6
Fredy <fredy.huachaca21@gmail.com>
2020-10-09 17:07:02 -0500

    Mostrando Peliculas en el swiper

--- lib/src/pages/home_page.dart
+++ lib/src/pages/home_page.dart
@@ -4,6 +4,8 @@ import 'package:peliculas/src/providers/peliculas_providers.dart';
 import 'package:peliculas/src/providers/peliculas_providers.dart';
 
 class HomePage extends StatelessWidget {
+  final peliculasProvider = new PeliculasProvider();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -26,11 +28,23 @@ class HomePage extends StatelessWidget {
   }
 
   Widget _swiperTarjetas() {
-    final peliculasProvider = new PeliculasProvider();
-    peliculasProvider.getEncines();
-
-    return CardSwiper(
-      peliculas: [1, 2, 3, 4, 5, 6, 7, 8, 9],
+    return FutureBuilder(
+      future: peliculasProvider.getEncines(),
+      // initialData: InitialData,
+      builder: (BuildContext context, AsyncSnapshot<List> snapshot) {
+        if (snapshot.hasData) {
+          return CardSwiper(peliculas: snapshot.data);
+        } else {
+          return Container(
+              height: 400.0, child: Center(child: CircularProgressIndicator()));
+        }
+      },
     );
+
+    // peliculasProvider.getEncines();
+
+    // return CardSwiper(
+    //   peliculas: [1, 2, 3, 4, 5, 6, 7, 8, 9],
+    // );
   }
 }
